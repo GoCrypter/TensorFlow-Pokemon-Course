4bf21c42ed63306b775e83cfcdd48315fb2922f4
unknown <goutam31893@gmail.com>
2021-01-07 18:12:58 +0530

    Inital Commit

---
+++ Setup_env.py
@@ -0,0 +1,10 @@
+
+# Tensorflow pokemon course
+# Install and setup environment-1
+
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
